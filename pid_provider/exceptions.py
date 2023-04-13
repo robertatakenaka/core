c10bba63e4e62af41e029988ce6011b5ef47629b
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-04-13 17:38:58 -0300

    Adiciona as exceções para pid_provider

---
+++ pid_provider/exceptions.py
@@ -0,0 +1,31 @@
+
+class APIPidProviderPostError(Exception):
+    ...
+
+
+class ForbiddenXMLDocPidRegistrationError(Exception):
+    ...
+
+
+class GetAPITokenError(Exception):
+    ...
+
+
+class NotEnoughParametersToGetDocumentRecordError(Exception):
+    ...
+
+
+class QueryDocumentMultipleObjectsReturnedError(Exception):
+    ...
+
+
+class XMLDocPidCreateError(Exception):
+    ...
+
+
+class XMLDocPidUpdateError(Exception):
+    ...
+
+
+class XMLDocPidXMLWithPreError(Exception):
+    ...
